36f03391e972697c83ff9eeec7c14161fbcfd71a
Alessandro Vinicius Siquiera Santos <alessandro.vncs@gmail.com>
2019-08-31 07:06:05 -0300

    Adicionado socket

--- index.js
+++ index.js
@@ -3,33 +3,43 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const uri = "mongodb+srv://Nugou:253726867@clustertest-kpogq.mongodb.net/test?retryWrites=true&w=majority";
-const port = 8080;
+const port = 3000;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = null;
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname ));
+//app.set('view engine','ejs');
+//app.set('views', __dirname + '/views');
+
 client.connect(err => {
     db = client.db("sensor");
     // perform actions on the collection object
-    app.listen(process.env.PORT || 3000, function () {
-        console.log('Server executando na porta ' + port);
+    app.listen(process.env.PORT || port, function () {
+        console.log('Server executando na porta ' + this.address().port);
     });
 
     //client.close();
 });
 
-app.use(bodyParser.json());
-
-app.use(express.static(__dirname + '/public'));
-app.set('view engine','ejs');
-app.set('views', __dirname + '/views');
-
+io.on('connection', () => {
+    console.log('socket on');
+});
 
 app.get('/', (req, res) => {
+    //res.render('show.ejs', { data: results });
+    //res.render('show.ejs');
+});
+
+app.get('/data', (req, res) => {
     db.collection('data').find().toArray((err, results) => {
         if (err) return console.log(err);
-        res.render('show.ejs', { data: results });
-        console.log(results);
+        //console.log(results);
+        res.send(results);
     });
 });
 
@@ -38,6 +48,7 @@ app.post('/register', (req, res) => {
     db.collection('data').insert(req.body, (err, result) => {
         if (err) return console.log(err);
 
+        io.emit('info', req.body);
         console.log('Salvo no banco');
         res.sendStatus(200);
     });
